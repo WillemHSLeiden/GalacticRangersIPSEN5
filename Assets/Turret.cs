fbc445614b3a5b094fcb445b4c90281db374a16a
Willem <s1109221@student.hsleiden.nl>
2020-05-13 14:27:45 +0200

    Added aim system
    Aiming is done by copying the rotation of the controller to the turret. Reticles are parented to the turret itself.

---
+++ Assets/Turret.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public Transform controller;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 aimDir = Vector3.RotateTowards(transform.position, controller.position - transform.position, 360, 0);
+        Quaternion aimDir = controller.rotation;
+        transform.rotation = Quaternion.Lerp(transform.rotation, aimDir, 0.2f);
+    }
+}
