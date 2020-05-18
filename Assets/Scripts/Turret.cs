9fe8addf5a7c6843904b24bb82470dc3daed60cc
Willem <s1109221@student.hsleiden.nl>
2020-05-18 11:38:18 +0200

    Push scene

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -9,7 +9,6 @@ public class Turret : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Vector3 aimDir = Vector3.RotateTowards(transform.position, controller.position - transform.position, 360, 0);
         Quaternion aimDir = controller.rotation;
         transform.rotation = Quaternion.Lerp(transform.rotation, aimDir, 0.2f);
     }
