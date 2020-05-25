9230cb33eabbaf1a0ed58844fe61690f05cc894a
JessePol <JessePoleij@hotmail.com>
2020-05-25 12:05:28 +0200

    Turret can move
    Move the turret with the WASD buttons.
    W for up
    D for down
    S for left
    D for right

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -11,14 +11,30 @@ public class Turret : MonoBehaviour{
     // Update is called once per frame
     void Update()
     {
+        Vector3 relativePos = controller.position - transform.position;
         Quaternion aimDir = controller.rotation;
         transform.rotation = Quaternion.Lerp(transform.rotation, aimDir, 0.2f);
-        
-        if (Input.GetKeyDown(KeyCode.Space)){
-            Debug.Log("Pressed Space so fire?");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             Instantiate(laserPrefab, transform.position, transform.localRotation);
         }
 
-       
+        //Move up or down for test
+        if (Input.GetKey(KeyCode.W)) {
+            controller.Rotate(-1, 0, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.S)){
+            controller.Rotate(1, 0, 0);
+        }
+        //Move left or right for test
+        if (Input.GetKey(KeyCode.A)){
+            controller.Rotate(0, -1, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.D)){
+            controller.Rotate(0, 1, 0);
+        }
     }
 }
