cfecb2e222d46a80e6b026325c5bf68282888c4e
Singh <s1110772@student.hsleiden.nl>
2020-05-25 17:15:52 +0200

    Commit StartLevel GUI

---
+++ Assets/MainItems.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //Nodig om van scenes te wisselen
+
+public class MainItems : MonoBehaviour
+{
+    // Starten van het level
+    public void StartLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
