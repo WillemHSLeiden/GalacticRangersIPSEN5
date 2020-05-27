c61da0fc5b38aa16e3aa886b57bd71c8d0975693
JessePol <JessePoleij@hotmail.com>
2020-05-27 16:24:15 +0200

    Astroid behavior

---
+++ Assets/Scripts/AstriodBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstriodBehavior : MonoBehaviour{
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Laser")
+        {
+            Destroy(collider.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/CollisionBehaviors/OnCollisionDestroy.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class DestroyOnCollision : MonoBehaviour, OnCollisionStrategy
-{
-    // Start is called before the first frame update
-    public void OnCollision(Collider collider){
-        Destroy(collider.gameObject);
-    }
-}
