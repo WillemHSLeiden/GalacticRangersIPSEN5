6d0ee2ab6a9c483e3ad8bc0859b32339d85814b7
JessePol <JessePoleij@hotmail.com>
2020-05-27 16:28:40 +0200

    Misspelled it
    now astroid instead of astriod.

--- Assets/Scripts/AstroidBehavior.cs
+++ Assets/Scripts/AstroidBehavior.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AstriodBehavior : MonoBehaviour{
+public class AstroidBehavior : MonoBehaviour{
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.tag == "Laser")
