d01187d9cbd37046d000a4b9861ae92e891c3b82
Cyriel <cyrielvdraaf@hotmail.com>
2020-05-27 14:10:16 +0200

    added little changes

---
+++ Assets/Scripts/LockOn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockOn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //gameObject.GetComponent<Renderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        //Check to see if the Collider's name is "Chest"
+        if (collision.collider.name == "Cube")
+        {
+            //Output the message
+            Debug.Log("Chest is here!");
+        }
+    }
+}
