80409225ffd521f7a010949e9fcdff4de510956e
JessePol <JessePoleij@hotmail.com>
2020-05-27 13:58:22 +0200

    Collision
    A collision system based on strategy pattern.\
    A meteor which has collision.

---
+++ Assets/Scripts/CollisionAction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class CollisionAction : MonoBehaviour{
+    [System.Serializable]
+    public class hitEvents : UnityEvent { }
+
+    [SerializeField]
+    public hitEvents hitEvent;
+
+    public OnCollisionStrategy onCollision;
+
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider collider) {
+        if (collider.tag == "laserPrefab") {
+            hitEvent.Invoke();
+            onCollision.OnCollision(collider);
+        }
+    }
+}
---
+++ Assets/Scripts/CollisionBehaviors/OnCollisionDestroy.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class DestroyOnCollision : MonoBehaviour, OnCollisionStrategy
+{
+    // Start is called before the first frame update
+    public void OnCollision(Collider collider){
+        Destroy(collider.gameObject);
+    }
+}
