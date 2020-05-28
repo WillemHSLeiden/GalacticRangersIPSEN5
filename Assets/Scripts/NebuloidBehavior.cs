cfb0bad70c096bd131e605e39f5f970fe901ec40
Willem <s1109221@student.hsleiden.nl>
2020-05-28 12:08:17 +0200

    Added dying effect to asteroid and enemies

--- Assets/Scripts/NebuloidBehavior.cs
+++ Assets/Scripts/NebuloidBehavior.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AstroidBehavior : MonoBehaviour{
+public class NebuloidBehavior : MonoBehaviour {
 
     [SerializeField]
     private Material flashMat;
 
-    private void OnTriggerEnter(Collider collider)
-    {
-        if (collider.tag == "Laser")
-        {
+    private void OnTriggerEnter(Collider collider) {
+        if (collider.tag == "Laser") {
             Destroy(collider.gameObject);
-            gameObject.GetComponent<Renderer>().material = flashMat;
+            transform.GetChild(0).GetComponent<Renderer>().material = flashMat;
             Invoke("SetInactive", 0.05f);
         }
     }
