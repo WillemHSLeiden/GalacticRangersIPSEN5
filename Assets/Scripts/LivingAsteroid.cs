758ceb9b470825524ad2544e76c457e977af12e6
Willem <s1109221@student.hsleiden.nl>
2020-06-08 14:08:20 +0200

    Created empty LivingAsteroid behavior

---
+++ Assets/Scripts/LivingAsteroid.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingAsteroid : MonoBehaviour
+{
+    public Animator anim;
+
+
+    public void Attack() {
+
+    }
+
+    public void FireLaser() {
+
+    }
+}
