1a3c7ec0b094325d303379fcfcea68a1b185f591
Cyriel <cyrielvdraaf@hotmail.com>
2020-06-10 22:56:10 +0200

    adding environment

---
+++ Assets/Scripts/LevelSpeed.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSpeed : MonoBehaviour
+{
+    [SerializeField] private float levelSpeed = 5f;
+
+    public float getLevelSpeed()
+    {
+        return levelSpeed;
+    }
+}
